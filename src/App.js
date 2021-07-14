e38462428d08b354050667798ed29b51ad0ecae4
Vanessadu <a787293136@gmail.com>
2021-07-14 16:09:03 -0400

    fixing the react router not suppoerted by github

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import Contact from "./pages/contact-page/contact.component";
 import MenuPage from "./pages/menu-page/menu-page.component";
 import MenuDetail from "./pages/menu-detail-page/menu-detail-page.component";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 import NevBar from "./components/nevbar/nevbar.component";
 
 class App extends React.Component {
@@ -12,12 +12,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <NevBar />
-        <Router basename={`${process.env.PUBLIC_URL}/`}>
+        <HashRouter basename={`${process.env.PUBLIC_URL}/`}>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/contact" component={Contact} />
           <Route path="/menu" component={MenuPage} />
           <Route path="/menu/:menuId" component={MenuDetail} />
-        </Router>
+        </HashRouter>
       </div>
     );
   }
