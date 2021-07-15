39a2cbd36dc4bed0dc762437707e3ebc1558398d
Vanessadu <a787293136@gmail.com>
2021-07-15 10:35:19 -0400

    fixing the BrowserRouter bug

--- src/components/nevbar/nevbar.component.jsx
+++ src/components/nevbar/nevbar.component.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 const NevBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand" href="#home">
         <Logo />
       </a>
       <button
@@ -21,18 +21,18 @@ const NevBar = () => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="/">
+            <a className="nav-link" href="#home">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
 
           <li className="nav-item">
-            <a className="nav-link" href="/contact">
+            <a className="nav-link" href="#contact">
               Contact
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/menu">
+            <a className="nav-link" href="#menu">
               Menu
             </a>
           </li>
